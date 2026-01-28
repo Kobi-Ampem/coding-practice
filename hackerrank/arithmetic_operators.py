c218a5059a0b17dc3a00f1113175f7ad5af54ff2
Kobi-Ampem <afriyieampemkobi@gmail.com>
2026-01-28 05:57:29 +0000

    Add arithmetic operators solution from hackerrank

---
+++ hackerrank/arithmetic_operators.py
@@ -0,0 +1,17 @@
+"""
+Platform: Hackerrank
+Language: Python
+Subdomain: Introduction
+Problem: Arithmetic operations
+"""
+
+#Print the sum, the difference and the product of two numbers, each on a new line.
+
+if __name__ = '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*c)
+
