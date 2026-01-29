c81f6f9b6df5ef7c5625b1a396a81e5484308354
Kobi-Ampem <afriyieampemkobi@gmail.com>
2026-01-29 05:45:14 +0000

    Add solution of division problem from hackerrank

---
+++ hackerrank/division.py
@@ -0,0 +1,16 @@
+"""
+Platform: Hackerrank
+Language: Python
+Subdomain: Introduction
+Problem: Division
+"""
+
+#Integer and float division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a//b)
+    print(a/b)
+
