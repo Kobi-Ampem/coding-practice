ce15b9e549351aec1fd4d330370712181a6fc1bb
Kobi-Ampem <afriyieampemkobi@gmail.com>
2026-01-30 06:38:33 +0000

    Add solution to write a function problem from hackerrank

---
+++ hackerrank/write_a_function.py
@@ -0,0 +1,22 @@
+"""
+Platform: Hackerrank
+Language: Python
+Subdomain: Introduction
+Problem: Write a function
+"""
+
+#A function that checks whether a year is leap or not.
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        leap = True
+    if year % 100 == 0:
+        leap = False
+    if year % 400 == 0:
+        leap = True    
+    return leap
+
+year = int(input())
+print(is_leap(year))
