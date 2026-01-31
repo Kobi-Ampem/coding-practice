6500531a68dfa7b5ac55c829707eb5ada1b24d29
Kobi-Ampem <afriyieampemkobi@gmail.com>
2026-01-31 07:12:52 +0000

    Add solution to loop problem on hackerrank

---
+++ hackerrank/loops.py
@@ -0,0 +1,15 @@
+"""
+Platform: Hackerrank
+Language: Python
+Subdomain: Introduction
+Problem: Loops
+"""
+
+#Using for loop
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i*i)
+
