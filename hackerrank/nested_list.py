158e23f92212a4d6e98ffd1899cafb046ca08431
Kobi-Ampem <afriyieampemkobi@gmail.com>
2026-02-11 12:53:28 +0000

    Add solution to nested list problem on hackerrank

---
+++ hackerrank/nested_list.py
@@ -0,0 +1,10 @@
+"""
+Platform = Hackerrank
+Language = Python
+Subdomain = Basic data types
+Problem = Nested lists
+
+"""
+
+#Print the name(s) of any student(s) with the second lowest grade. order names alphabetically and print each on a new line
+
